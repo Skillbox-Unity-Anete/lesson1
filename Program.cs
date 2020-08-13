83f8ab7908c38be85312109dcc6de1a7ed17049b
anete.anetes <anete.anetes@gmail.com>
2020-08-13 16:10:10 +0300

    * добавлено решение задания 1 и 2

--- Program.cs
+++ Program.cs
@@ -9,10 +9,28 @@ namespace Homework_01
 {
     class Program
     {
+        private static Random r = new Random();
+
         static void Main(string[] args)
         {
-            // Создание базы данных из 20 сотрудников
-            Repository repository = new Repository(50);
+            //№1
+            Repository repository = new Repository(r.Next(1, 20));
+            repository.Print("База данных до преобразования");
+            repository.DeleteWorkerBySalary(30000);
+            repository.Print("База данных после первого преобразования");
+
+            //№2
+            repository = new Repository(40);
+            repository.Print("База данных до преобразования");
+            repository.Workers.Take(20).ToList().ForEach(x =>
+            {
+                repository.Workers.Remove(x);
+            });
+            repository.Print("База данных после первого преобразования");
+
+
+            //№3
+            repository = new Repository(50);
 
             // Печать в консоль всех сотрудников
             repository.Print("База данных до преобразования");
